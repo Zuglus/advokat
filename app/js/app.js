33c43ba7f0e1b9c6323951dec3fcd74f097271db
Andrey <kynterets@gmail.com>
2020-07-06 00:00:39 +0400

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,11 @@ window.addEventListener('load', () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('../sw.js')
-    .then(registration => {
+    .then((registration) => {
       console.log('Service worker successfully registered', registration);
     })
-    .catch(console => {
-      console.log('Service worker registration failed', error);
+    .catch((error) => {
+      console.log('Service worker registration failed' + error);
     });
   }
 });
--- app/sw.js
+++ app/sw.js
@@ -1,40 +1,51 @@
-const staticCacheName = 'static-cache-v0';
+const cacheName = 'v1';
+const cacheKeeplist = ['v2'];
 
-const staticAssets = [
-  '/',
-  '/index.html',
-  '/contacts.html',
-  '/services.html',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-192x192.png',
-  '/images/location.svg',
-  '/images/phone.svg',
-  '/images/email.svg',
-  '/css/style.css',
-  '/js/app.js',
-  '/js/index.js'
-];
-
-self.addEventListener('install', async event => {
-  const cache = await caches.open(staticCacheName);
-  await cache.addAll(staticAssets);
-  console.log('Service worker has been installed');
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'contacts.html',
+        'services.html',
+        'images/icons/icon-128x128.png',
+        'images/icons/icon-192x192.png',
+        'images/location.svg',
+        'images/logo.svg',
+        'images/telegram.svg',
+        'images/train.svg',
+        'images/viber.svg',
+        'images/whatsapp.svg',
+        'images/external-link.svg',
+        'images/phone.svg',
+        'images/email.svg',
+        'css/style.css',
+        'js/app.js'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', async event => {
-  const cachesKeys = await caches.keys();
-  const checkKeys = cachesKeys.map(async key => {
-   if (staticCacheName !== key) {
-    await caches.delete(key);
-  }
-  });
-  await Promise.all(checkKeys);
-  console.log('Service worker has been activated');
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  console.log(`Trying to fetch ${event.request.url}`);
-  event.respondWith(caches.match(event.request).then(cachedResponse => {
-    return cachedResponse || fetch(event.request)
-  }));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then(async (response) => {
+        const cache = await caches.open(cacheName);
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
 });
